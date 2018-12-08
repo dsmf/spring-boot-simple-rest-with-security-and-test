0de3296bec10a792ae63b106465aad9eb1fe8c9e
Fischer Matthias <Matthias.Fischer@doubleSlash.de>
2018-12-08 01:47:57 +0100

    Add simple REST with security and test

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/hello")
+    public Hello greeting(@RequestParam(value="name") String name) {
+        return new Hello(counter.incrementAndGet(),
+                           "Hello " + name);
+    }
+    
+    @RequestMapping("/admin")
+    public Hello adminHello(@RequestParam(value="name") String name) {
+        return new Hello(counter.incrementAndGet(), "Hello admin " + name);
+    }
+}
---
+++ src/main/java/hello/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable() //
+				.authorizeRequests() //
+				.antMatchers("/admin/**").hasRole("ADMIN") //
+				.antMatchers("/hello/**").hasAnyRole("USER", "ADMIN") //
+				.and().httpBasic();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// allow Pre-flight [OPTIONS] request from browser
+		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+}
